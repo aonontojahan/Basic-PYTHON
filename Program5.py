b5c3d2587799ec55cedd95c1c9121e3178b18d7f
aonontojahan <aonontojahan@gmail.com>
2025-07-26 04:20:51 +0600

    TypeCausting

---
+++ Program5.py
@@ -0,0 +1,16 @@
+#Typecausting
+num1 = input("Enter the first number ")
+num2 = input("Enter the secound number ")
+
+sum = int(num1) + int(num2)
+mul = int(num1) * int(num2)
+div = float(num1) / float(num2)
+exp = float(num1) ** float(num2)
+reminder = float(num1) % float(num2)
+
+print("The given values output are: ")
+print("The sum of the given number is ",sum)
+print("The multiplication of the given number is ",mul)
+print("The division of the given number is ",div)
+print("The exponential of the given number is ",exp)
+print("The reminder of the given number is ",reminder)
