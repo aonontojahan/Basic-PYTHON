a558ba20a79e8d911c9c605298fa377ea728f0b3
aonontojahan <aonontojahan@gmail.com>
2025-07-26 19:22:59 +0600

    Practicing Previous Code

---
+++ Program9.py
@@ -0,0 +1,17 @@
+from math import *
+print("Hello")
+
+name = input("Please enter your name: ")
+number1 = int(input("Please enter an integer number: "))
+number2 = int(input("Please enter another integer number: "))
+
+height = float(input("Please enter the value for height: "))
+base = float(input("Please enter the value for base: "))
+area_triangle = 0.5*height*base
+
+
+sum=number1+number2
+print("Your name is: "+name)
+print("The summation of your's input number is: ",sum)
+print("The max number of your's input number is: ",max(number1,number2))
+print("The area of the tri-angle is: ",area_triangle)
