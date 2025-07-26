a0c6104012e0bd0d95950cbff35469d41cc19ebe
aonontojahan <aonontojahan@gmail.com>
2025-07-27 02:26:00 +0600

    Relational Operator

---
+++ Program12.py
@@ -0,0 +1,10 @@
+#Relational Operator and bollean
+
+number1=int(input("Please enter the firdt number: "))
+number2=int(input("Please enter the secound number: "))
+
+result=number1>number2
+check=number1<number2
+print("The statement is: ",result)
+print("The statement is: ",number1==number2)
+print("The satement is: ",check)
